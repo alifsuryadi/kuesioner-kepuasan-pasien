6e5843d46ea89ada1963d5893e9ac847d5c0b8f0
Alif Suryadi <alifsuryadi037@gmail.com>
2024-02-10 23:51:52 +0700

    fix bug: change glightbox href & rename slider-answer.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,10 +1,3 @@
-/**
- * Template Name: Yummy
- * Updated: Jan 09 2024 with Bootstrap v5.3.2
- * Template URL: https://bootstrapmade.com/yummy-bootstrap-restaurant-website-template/
- * Author: BootstrapMade.com
- * License: https://bootstrapmade.com/license/
- */
 document.addEventListener("DOMContentLoaded", () => {
   "use strict";
 
